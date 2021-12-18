685424834ee0a0a8466deb97f9e5dcad5e677538
최승식(ChoiSeungSik) <tmdtlr0911@naver.com>
2021-12-18 23:43:18 +0900

    주석추가

--- Wastewater exchange/realT_Video.py
+++ Wastewater exchange/realT_Video.py
@@ -10,11 +10,11 @@ while(True):
         print('▶ 1 : PH 예측시작(종료시 q버튼)')
         print('▶ 2 : 기타 ~')
         print('▶ 0 : 프로그램 종료\n')
-        choice = int(input('번호를 선택하십시오 : '))
+        choice = int(input('번호를 선택하십시오 : ')) # 번호를 입력받아서 choice의 변수를 담음
         print()
 
     except Exception as ex:
-        print('error :', ex)
+        print('error :', ex) # 예외처리 구문 오류가 생겼을때 Error를 출력
         print()
         choice = None
         time.sleep(1)
@@ -22,16 +22,16 @@ while(True):
     if choice == None:
         continue
     
-    elif choice == 0:
+    elif choice == 0: # choice의 선ㅌㄱ이 0일 경우에 프로그램을 종료시킴
         print('프로그램이 3초뒤 종료됩니다.')
         time.sleep(3)
         break
 
-    elif choice == 1:
-        img_path = './realT_img/'
-        pred_path = './realT_crop/'
+    elif choice == 1: # choice가 1일 경우에 PH측정 프로그램을 구동시킴
+        img_path = './realT_img/' # 이미지의 주소를 설정
+        pred_path = './realT_crop/' # predict할 이미지 주소를 설정
 
-        pred_model = predict.PH_predict(pred_path)
+        pred_model = predict.PH_predict(pred_path) # PH측정 시작
         cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
         os.system('cls')
@@ -55,13 +55,13 @@ while(True):
             
             sec = int(time.time()-start)
 
-            if sec >= 60:
+            if sec >= 60: # 실행을 시키고 1분지날경우 min에 1분을 추가하고 시간을 출력함
                 temp_m= sec // 60
                 min += temp_m
                 start = time.time() # 이 과정에서 n초의 손실 발생 
                 print('[ run time : {}시간 {}분 ]\n'.format(hour, min))
             
-            if min >= 60: 
+            if min >= 60: # 실행을시키고 1시간이 지날경우 hour에 1을 추가하고 min을 0으로 만듬
                 hour += 1
                 min = 0
                 print('[ run time : {}시간 {}분 ]\n'.format(hour, min))
@@ -91,10 +91,3 @@ while(True):
                 break
         print('break')
         break
-
-
-
-                
-                
-
-
